bc584318f9aff6b49a62cd0a2b8ef4c595720c9c
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2024-04-24 15:08:57 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -86,7 +86,7 @@ export const Anthropic = {
 };
 
 export const OpenaiPath = {
-  ChatPath: "v1/chat/completions",
+  ChatPath: "chat/completions",
   UsagePath: "dashboard/billing/usage",
   SubsPath: "dashboard/billing/subscription",
   ListModelPath: "v1/models",
